0b0aef9da44d07cc0557bad8a13a65fca5602350
Sergei Korchagin <gamestationbase@gmail.com>
2023-04-21 19:16:31 +0300

    add logic in show list with notes

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,61 @@
 const input = document.querySelector('.input');
-const button = document.querySelector('.btn');
+const buttonAddNote = document.querySelector('.btn-add');
 const listNotes = document.querySelector('.list-notes');
+const buttonCloseAll = document.querySelector('.btn-close-all');
+const buttonSaveNotes = document.querySelector('.save-notes');
+const inputSaveNotes = document.querySelector('.name-save-notes');
 
-button.addEventListener(
+const viewingList = document.querySelector('.viewing-notes');
+
+buttonAddNote.addEventListener(
   'click',
   () => {
-    if(input.value === '') return;
-    if(listNotes.children.length === 30) return;
-    let html = `<div class = 'note'><li class = 'list-item'>${input.value}</li><button class = 'btn-close'>Close</div>`;
-    listNotes.insertAdjacentHTML('beforeend', html);
-  }
+    if (input.value === '') return;
+    if (listNotes.children.length === 20) return;
+    if (listNotes.children) buttonCloseAll.style.display = 'block';
+
+    const elementNote = document.createElement('div');
+    elementNote.classList.add('note');
+
+    elementNote.insertAdjacentHTML(
+      'beforeend',
+      `<li class = 'list-item'>${input.value}</li><button class='btn-close'>Close</button>`,
+    );
+
+    listNotes.append(elementNote);
+    const btnClose = elementNote.querySelector('.btn-close');
+    btnClose.addEventListener(
+      'click',
+      (ev) => {
+        const element = ev.target.closest('.note');
+        element.remove();
+      },
+    );
+
+    const arrListNode = Array.from(document.querySelectorAll('.note'));
+    buttonCloseAll.addEventListener(
+      'click',
+      () => {
+        buttonCloseAll.style.display = 'none';
+        arrListNode.forEach((item) => {
+          item.remove();
+        });
+      },
+    );
+
+let arrValueItems = [];
+arrListNode.forEach((item) => {
+  arrValueItems.push(item.firstChild.textContent);
+  return arrValueItems;
+});
+
+    buttonSaveNotes.addEventListener(
+      'click',
+      () => {
+        let namePosition = inputSaveNotes.value
+        localStorage.setItem(namePosition, arrValueItems);
+        console.log(namePosition)
+      }
+    );
+  },
 );
